9741a55a70bf2861a3de4e81cd87cc0a21dbbc0a
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-13 12:37:34 +0530

    commit

---
+++ src/main/java/edu/RestaurantManagement/model/UserModel.java
@@ -0,0 +1,15 @@
+package edu.RestaurantManagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserModel {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
