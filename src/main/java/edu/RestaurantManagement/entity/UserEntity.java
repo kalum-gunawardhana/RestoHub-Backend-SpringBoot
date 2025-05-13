644c9ff0ff1a3e310daa7823b11ff5fc5a48b147
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-13 12:28:13 +0530

    commit

---
+++ src/main/java/edu/RestaurantManagement/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package edu.RestaurantManagement.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
